f2fb329a7f6b1de450955f85284724c89a883eae
Chanwon-Seo <scwonn60@gmail.com>
2024-07-20 19:01:02 +0900

    init commit

---
+++ src/main/java/com/b1/sycls/SyclsApplication.java
@@ -0,0 +1,13 @@
+package com.b1.sycls;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SyclsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SyclsApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/b1/sycls/SyclsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.b1.sycls;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SyclsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
