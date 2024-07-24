ac122ccf429e0794d94f1a9f9d8e94a2fcc29812
Chanwon-Seo <scwonn60@gmail.com>
2024-07-25 01:00:33 +0900

    🩹 fix(#18): 공통 상수 클래스 추가

--- sycls-common/src/main/java/com/b1/constant/DomainConstant.java
+++ sycls-common/src/main/java/com/b1/constant/DomainConstant.java
@@ -3,4 +3,5 @@ package com.b1.constant;
 public abstract class DomainConstant {
 
     public static final Long SEAT_RESERVATION_TIME = 1L;
+    public static final int MAXIMUM_SELECTED_SEAT = 4;
 }
