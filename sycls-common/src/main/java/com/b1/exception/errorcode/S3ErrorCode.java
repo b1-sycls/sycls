12ddf22b6f5fbd18286fc26f70638253b296458a
Cho Koung Min <andy123423@naver.com>
2024-07-26 00:49:49 +0900

    ♻️ refactor: 에러코드 상태코드 수정

--- sycls-common/src/main/java/com/b1/exception/errorcode/S3ErrorCode.java
+++ sycls-common/src/main/java/com/b1/exception/errorcode/S3ErrorCode.java
@@ -9,7 +9,7 @@ import org.springframework.http.HttpStatus;
 public enum S3ErrorCode implements ErrorCode {
 
     S3_INVALID_IMAGE_TYPE(HttpStatus.BAD_REQUEST.value(), "이미지 타입이 맞지 않습니다."),
-    S3_MISSING_IMAGE(HttpStatus.BAD_REQUEST.value(), "이미지가 누락되었습니다."),
+    S3_MISSING_IMAGE(HttpStatus.NOT_FOUND.value(), "이미지가 누락되었습니다."),
     S3_UPLOADING_FAIL(HttpStatus.BAD_REQUEST.value(), "이미지 업로드 중 오류가 발생하였습니다."),
     ;
 
