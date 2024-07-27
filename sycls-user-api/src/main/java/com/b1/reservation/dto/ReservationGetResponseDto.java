788f6de056fcd6de4bb3ca0def1c058b98185a04
Chanwon-Seo <scwonn60@gmail.com>
2024-07-27 19:22:57 +0900

    🎉 feat(#89): 예매 조회 응답 점유ID 업데이트

--- sycls-user-api/src/main/java/com/b1/reservation/dto/ReservationGetResponseDto.java
+++ sycls-user-api/src/main/java/com/b1/reservation/dto/ReservationGetResponseDto.java
@@ -2,7 +2,6 @@ package com.b1.reservation.dto;
 
 import com.b1.round.entity.Round;
 import com.b1.seat.entity.SeatReservationLog;
-import com.b1.seatgrade.entity.SeatGrade;
 import java.util.HashSet;
 import java.util.Map;
 import java.util.Set;
@@ -18,18 +17,26 @@ import lombok.Getter;
 public class ReservationGetResponseDto {
 
     private final Long roundId;
+    private final Set<Long> reservationIds;
     private final Set<Long> seatGradeIds;
     private final Set<String> seatCodes;
 
     public static ReservationGetResponseDto of(
             final Round selectedRound,
             final Set<SeatReservationLog> findSeatReservationLogs) {
+
         Map<Long, String> seatGradeMap = findSeatReservationLogs.stream()
-                .collect(Collectors.toMap(srl -> srl.getSeatGrade().getId(),
+                .collect(Collectors.toMap(
+                        srl -> srl.getSeatGrade().getId(),
                         srl -> srl.getSeatGrade().getSeat().getCode()));
+
+        Set<Long> reservationIds = findSeatReservationLogs.stream()
+                .map(SeatReservationLog::getId)
+                .collect(Collectors.toSet());
 
         return ReservationGetResponseDto.builder()
                 .roundId(selectedRound.getId())
+                .reservationIds(reservationIds)
                 .seatGradeIds(seatGradeMap.keySet())
                 .seatCodes(new HashSet<>(seatGradeMap.values()))
                 .build();
