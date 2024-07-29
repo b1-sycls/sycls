43142332d3f1dfd458fcd8de94e61661968501a7
Chanwon-Seo <scwonn60@gmail.com>
2024-07-29 12:48:26 +0900

    🎉 feat(#101): security CORS 추가

--- sycls-user-api/src/main/java/com/b1/config/WebSecurityConfig.java
+++ sycls-user-api/src/main/java/com/b1/config/WebSecurityConfig.java
@@ -1,12 +1,8 @@
 package com.b1.config;
 
-import com.b1.security.JwtAuthenticationFilter;
-import com.b1.security.JwtAuthorizationFilter;
-import com.b1.security.JwtLogoutHandler;
-import com.b1.security.JwtLogoutSuccessHandler;
-import com.b1.security.JwtProvider;
-import com.b1.security.UserDetailsServiceImpl;
+import com.b1.security.*;
 import com.b1.user.UserHelper;
+import jakarta.servlet.http.HttpServletRequest;
 import lombok.RequiredArgsConstructor;
 import org.springframework.boot.autoconfigure.security.servlet.PathRequest;
 import org.springframework.context.annotation.Bean;
@@ -15,10 +11,17 @@ import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.config.annotation.authentication.configuration.AuthenticationConfiguration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
 import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.web.SecurityFilterChain;
 import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
 import org.springframework.security.web.authentication.logout.LogoutFilter;
+import org.springframework.web.cors.CorsConfiguration;
+import org.springframework.web.cors.CorsConfigurationSource;
+import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
+
+import java.util.Collections;
+import java.util.List;
 
 @Configuration
 @EnableWebSecurity // Spring Security 지원을 가능하게 함
@@ -54,24 +57,47 @@ public class WebSecurityConfig {
     public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
         // CSRF 설정
         http.csrf((csrf) -> csrf.disable());
+        http.formLogin(AbstractHttpConfigurer::disable);
+        http.httpBasic(AbstractHttpConfigurer::disable);
+        http.cors(corsConfigurer -> corsConfigurer.configurationSource(new CorsConfigurationSource() {
+            @Override
+            public CorsConfiguration getCorsConfiguration(HttpServletRequest request) {
+                CorsConfiguration config = new CorsConfiguration();
+                config.setAllowedOrigins(Collections.singletonList("http://localhost:9090"));
+                config.setAllowedMethods(Collections.singletonList("*"));
+                config.setAllowCredentials(true);
+                config.setAllowedHeaders(Collections.singletonList("*"));
+                config.setMaxAge(3600L); //1시간
+                return config;
+            }
+        }));
+
 
         // 기본 설정인 Session 방식은 사용하지 않고 JWT 방식을 사용하기 위한 설정
         http.sessionManagement((sessionManagement) ->
                 sessionManagement.sessionCreationPolicy(SessionCreationPolicy.STATELESS)
         );
-
         http.authorizeHttpRequests((authorizeHttpRequests) ->
                 authorizeHttpRequests
-                        .requestMatchers(PathRequest.toStaticResources().atCommonLocations())
-                        .permitAll()
+                        .requestMatchers(PathRequest.toStaticResources().atCommonLocations()).permitAll() // resources 접근 허용 설정
+                        .requestMatchers("/favicon.ico").permitAll()
+
+                        // 정적 파일
+                        .requestMatchers("/static/**").permitAll()
+                        .requestMatchers("/templates/**").permitAll()
+                        .requestMatchers("/static/css/**").permitAll()
+
                         .requestMatchers("/v1/users/signup").permitAll()
                         .requestMatchers("/v1/auth/login").permitAll()
                         //임시 권한 수정
                         .requestMatchers("/v1/users/**").permitAll()
                         .requestMatchers("/v1/auth/**").permitAll()
-
-                        //test
-                        .requestMatchers("/v1/**").permitAll()
+                        // Payment
+                        .requestMatchers("/v1/payment").permitAll()
+                        .requestMatchers("/v1/payment/confirm").permitAll()
+                        .requestMatchers("/v1/payment/success").permitAll()
+                        .requestMatchers("/v1/payment/fail/**").permitAll()
+                        .requestMatchers("/v1/payment/client-key").permitAll()
 
                         .requestMatchers("/error").permitAll()
                         .anyRequest().authenticated()
