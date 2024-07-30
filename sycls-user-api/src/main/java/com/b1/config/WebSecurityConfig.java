22c99383640bb61371f3d9b47aa28e2047fdc48a
Cho Koung Min <andy123423@naver.com>
2024-07-31 02:12:19 +0900

    ♻️ refactor: User-api 필요한 Get요청 접근 허용

--- sycls-user-api/src/main/java/com/b1/config/WebSecurityConfig.java
+++ sycls-user-api/src/main/java/com/b1/config/WebSecurityConfig.java
@@ -7,12 +7,12 @@ import com.b1.security.JwtLogoutSuccessHandler;
 import com.b1.security.JwtProvider;
 import com.b1.security.UserDetailsServiceImpl;
 import com.b1.user.UserHelper;
-import jakarta.servlet.http.HttpServletRequest;
-import java.util.Collections;
+import java.util.List;
 import lombok.RequiredArgsConstructor;
 import org.springframework.boot.autoconfigure.security.servlet.PathRequest;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.http.HttpMethod;
 import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.config.annotation.authentication.configuration.AuthenticationConfiguration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
@@ -24,6 +24,7 @@ import org.springframework.security.web.authentication.UsernamePasswordAuthentic
 import org.springframework.security.web.authentication.logout.LogoutFilter;
 import org.springframework.web.cors.CorsConfiguration;
 import org.springframework.web.cors.CorsConfigurationSource;
+import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
 
 @Configuration
 @EnableWebSecurity // Spring Security 지원을 가능하게 함
@@ -55,26 +56,30 @@ public class WebSecurityConfig {
         return new JwtAuthorizationFilter(jwtProvider, userDetailsService);
     }
 
+    // CORS
+    @Bean
+    public CorsConfigurationSource corsConfigurationSource() {
+        CorsConfiguration configuration = new CorsConfiguration();
+        configuration.setAllowedOriginPatterns(List.of("*"));
+        configuration.addAllowedHeader("*");
+        configuration.addAllowedMethod("*");
+        configuration.setAllowCredentials(true);
+        configuration.addExposedHeader("Authorization"); // 클라이언트에서 접근할 수 있게 허용할 헤더 추가
+        configuration.addExposedHeader("RefreshToken"); // 클라이언트에서 접근할 수 있게 허용할 헤더 추가
+
+        UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
+        source.registerCorsConfiguration("/**", configuration);
+        return source;
+    }
+
     @Bean
     public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
         // CSRF 설정
         http.csrf((csrf) -> csrf.disable());
         http.formLogin(AbstractHttpConfigurer::disable);
         http.httpBasic(AbstractHttpConfigurer::disable);
-        http.cors(
-                corsConfigurer -> corsConfigurer.configurationSource(new CorsConfigurationSource() {
-                    @Override
-                    public CorsConfiguration getCorsConfiguration(HttpServletRequest request) {
-                        CorsConfiguration config = new CorsConfiguration();
-                        config.setAllowedOrigins(
-                                Collections.singletonList("http://localhost:9090"));
-                        config.setAllowedMethods(Collections.singletonList("*"));
-                        config.setAllowCredentials(true);
-                        config.setAllowedHeaders(Collections.singletonList("*"));
-                        config.setMaxAge(3600L); //1시간
-                        return config;
-                    }
-                }));
+        http.cors(corsConfigurer -> corsConfigurer.configurationSource(corsConfigurationSource()));
+
         // 기본 설정인 Session 방식은 사용하지 않고 JWT 방식을 사용하기 위한 설정
         http.sessionManagement((sessionManagement) ->
                 sessionManagement.sessionCreationPolicy(SessionCreationPolicy.STATELESS)
@@ -83,17 +88,40 @@ public class WebSecurityConfig {
         http.authorizeHttpRequests((authorizeHttpRequests) ->
                 authorizeHttpRequests
                         .requestMatchers(PathRequest.toStaticResources().atCommonLocations())
-                        .permitAll()
+                        .permitAll() // resources 접근 허용 설정
+                        .requestMatchers("/favicon.ico").permitAll()
+
+                        // 정적 파일
+                        .requestMatchers("/static/**").permitAll()
+                        .requestMatchers("/templates/**").permitAll()
+                        .requestMatchers("/static/css/**").permitAll()
+
                         .requestMatchers("/v1/users/signup").permitAll()
                         .requestMatchers("/v1/auth/login").permitAll()
                         //임시 권한 수정
                         .requestMatchers("/v1/users/**").permitAll()
                         .requestMatchers("/v1/auth/**").permitAll()
-
-                        //test
-                        .requestMatchers("/v1/**").permitAll()
+                        // Payment
+                        .requestMatchers("/v1/payment").permitAll()
+                        .requestMatchers("/v1/payment/confirm").permitAll()
+                        .requestMatchers("/v1/payment/success").permitAll()
+                        .requestMatchers("/v1/payment/fail/**").permitAll()
 
                         .requestMatchers("/error").permitAll()
+
+                        // Content
+                        .requestMatchers(HttpMethod.GET, "/v1/contents/**").permitAll()
+
+                        // Category
+                        .requestMatchers(HttpMethod.GET, "/v1/categories").permitAll()
+
+                        // Cast
+                        .requestMatchers(HttpMethod.GET, "/v1/casts").permitAll()
+
+                        // Round
+                        .requestMatchers(HttpMethod.GET, "/v1/rounds/**").permitAll()
+
+                        // ETC .. 필요한거 추가해서 사용하세요
                         .anyRequest().authenticated()
         );
 
