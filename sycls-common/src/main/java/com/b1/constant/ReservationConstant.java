9f3d4ddc665f0eb4f12bf154d6d273772eb6d1a2
Chanwon-Seo <scwonn60@gmail.com>
2024-08-01 17:00:18 +0900

    ♻️ refactor(#161): DoaminConstant -> ReservationConstant 클래스명 변경

--- sycls-common/src/main/java/com/b1/constant/ReservationConstant.java
+++ sycls-common/src/main/java/com/b1/constant/ReservationConstant.java
@@ -1,6 +1,6 @@
 package com.b1.constant;
 
-public abstract class DomainConstant {
+public abstract class ReservationConstant {
 
     public static final Long SEAT_RESERVATION_TIME = 5L;
     public static final int MAXIMUM_SELECTED_SEAT = 4;
