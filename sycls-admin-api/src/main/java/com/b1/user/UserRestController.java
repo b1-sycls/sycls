ad337d1ff35a5b8c2af868cc326095dd30f789df
leejinuk <leejinouk123@kakao.com>
2024-08-04 21:15:12 +0900

    ♻️ refactor(#172): 회원탈퇴 HTTP 메서드 Delete에서 Patch로 변경

--- sycls-admin-api/src/main/java/com/b1/user/UserRestController.java
+++ sycls-admin-api/src/main/java/com/b1/user/UserRestController.java
@@ -10,8 +10,8 @@ import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.core.annotation.AuthenticationPrincipal;
-import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PatchMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -43,7 +43,7 @@ public class UserRestController {
      *
      * @param requestDto : password
      */
-    @DeleteMapping("/users/resign")
+    @PatchMapping("/users/resign")
     public ResponseEntity<RestApiResponseDto<String>> resign(
             @Valid @RequestBody final UserResignRequestDto requestDto,
             @AuthenticationPrincipal final UserDetailsImpl userDetails
