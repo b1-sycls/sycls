ea1f38fb23048236d990a31d43e63d80c5ae3b3a
Chanwon-Seo <scwonn60@gmail.com>
2024-08-06 18:15:49 +0900

    ♻️ refactor(#208): Redis 설정

--- sycls-core/src/main/java/com/b1/config/RedisConfig.java
+++ sycls-core/src/main/java/com/b1/config/RedisConfig.java
@@ -1,5 +1,8 @@
 package com.b1.config;
 
+import org.redisson.Redisson;
+import org.redisson.api.RedissonClient;
+import org.redisson.config.Config;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
@@ -22,6 +25,8 @@ public class RedisConfig {
     @Value("${spring.data.redis.port}")
     private int port;
 
+    private static final String REDISSON_HOST_PREFIX = "redis://";
+
 
     @Bean
     public RedisConnectionFactory redisConnectionFactory() {
@@ -43,4 +48,13 @@ public class RedisConfig {
         template.setValueSerializer(new GenericJackson2JsonRedisSerializer());
         return template;
     }
+
+    @Bean
+    public RedissonClient redissonClient() {
+        Config config = new Config();
+        config.useSingleServer()
+                .setAddress(REDISSON_HOST_PREFIX + host + ":" + port);
+        return Redisson.create(config);
+    }
+
 }
