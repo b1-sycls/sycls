8f1e9af2f50a977ae55aec22ebff9408815e30dd
Chanwon-Seo <scwonn60@gmail.com>
2024-08-12 06:31:31 +0900

    🐛 bugfix(#254): 클래스명 변경

--- sycls-common/src/main/java/com/b1/exception/errorcode/ReservationErrorCode.java
+++ sycls-common/src/main/java/com/b1/exception/errorcode/ReservationErrorCode.java
@@ -6,7 +6,7 @@ import org.springframework.http.HttpStatus;
 
 @Getter
 @RequiredArgsConstructor
-public enum SeatReservationLogErrorCode implements ErrorCode {
+public enum ReservationErrorCode implements ErrorCode {
 
     SEAT_RESERVATION_NOT_AVAILABLE(HttpStatus.BAD_REQUEST.value(), "이미 예매 된 좌석입니다."),
     SEAT_RESERVATION_ALREADY_DISABLE(HttpStatus.BAD_REQUEST.value(), "이미 취소 된 예매 정보입니다."),
