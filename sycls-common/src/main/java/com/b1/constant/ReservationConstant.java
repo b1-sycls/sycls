ee59ca507282de7ebcba8b4d44a9e6d768136ea5
Chanwon-Seo <scwonn60@gmail.com>
2024-08-12 06:28:16 +0900

    🐛 bugfix(#254): 중복 클래스 제거 및 병합

--- sycls-common/src/main/java/com/b1/constant/ReservationConstant.java
+++
@@ -1,7 +0,0 @@
-package com.b1.constant;
-
-public abstract class ReservationConstant {
-
-    public static final Long SEAT_RESERVATION_TIME = 5L;
-    public static final int MAXIMUM_SELECTED_SEAT = 4;
-}
