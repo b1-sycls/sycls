a51963c5efe58095bf6b564ad29b3ba128e6d06b
Cho Koung Min <andy123423@naver.com>
2024-08-18 01:54:42 +0900

    🐛 bugfix(#306): s3 용랑제한 상향

--- sycls-common/src/main/java/com/b1/exception/exceptionhandler/ValidExceptionHandler.java
+++ sycls-common/src/main/java/com/b1/exception/exceptionhandler/ValidExceptionHandler.java
@@ -1,21 +1,35 @@
 package com.b1.exception.exceptionhandler;
 
 import com.b1.exception.errorcode.CommonErrorCode;
+import com.b1.exception.errorcode.S3ErrorCode;
 import com.b1.globalresponse.ErrorResponseDto;
 import java.util.concurrent.ConcurrentHashMap;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.core.NestedExceptionUtils;
 import org.springframework.core.annotation.Order;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.validation.FieldError;
 import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
+import org.springframework.web.multipart.MaxUploadSizeExceededException;
 
-@Slf4j
+@Slf4j(topic = "Valid Exception Handler")
 @Order(1)
 @RestControllerAdvice
 public class ValidExceptionHandler {
+
+    @ExceptionHandler(MaxUploadSizeExceededException.class)
+    public ResponseEntity<ErrorResponseDto> imageMaxUpSizeExceeded(
+            MaxUploadSizeExceededException e) {
+        log.info("[MaxUploadSizeExceededException] cause : {}, message : {} ",
+                NestedExceptionUtils.getMostSpecificCause(e), e.getMessage());
+
+        ErrorResponseDto errorResponseDto = ErrorResponseDto.of(S3ErrorCode.S3_IMAGE_SIZE_MAX);
+        return ResponseEntity.status(HttpStatus.BAD_REQUEST.value()).body(errorResponseDto);
+    }
+
 
     /**
      * 유효성 검사에서 예외가 발생
@@ -37,5 +51,4 @@ public class ValidExceptionHandler {
                 .status(CommonErrorCode.BAD_REQUEST.getHttpStatusCode())
                 .body(errorResponseDto);
     }
-
 }
