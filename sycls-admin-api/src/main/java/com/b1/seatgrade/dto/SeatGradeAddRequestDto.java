4a1242479821c06203fb699f838496142acb2b20
momoysm <90163578+momoysm@users.noreply.github.com>
2024-08-21 16:58:32 +0900

    🐛 bugfix(#322): 좌석 등급 등록시 가격 음수 입력 가능 수정

--- sycls-admin-api/src/main/java/com/b1/seatgrade/dto/SeatGradeAddRequestDto.java
+++ sycls-admin-api/src/main/java/com/b1/seatgrade/dto/SeatGradeAddRequestDto.java
@@ -1,6 +1,8 @@
 package com.b1.seatgrade.dto;
 
 import com.b1.seatgrade.entity.SeatGradeType;
+import jakarta.validation.constraints.Max;
+import jakarta.validation.constraints.Min;
 import jakarta.validation.constraints.NotEmpty;
 import jakarta.validation.constraints.NotNull;
 import java.util.List;
@@ -16,6 +18,8 @@ public record SeatGradeAddRequestDto(
         List<Long> seatIdList,
 
         @NotNull(message = "가격이 누락되었습니다.")
+        @Max(value = 1000000000, message = "가격이 10억원을 넘을 수 없습니다.")
+        @Min(value = 0, message = "가격이 0원보다 적을 수 없습니다.")
         Integer price
 ) {
 
