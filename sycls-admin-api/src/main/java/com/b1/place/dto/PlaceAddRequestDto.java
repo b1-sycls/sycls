3381c1758c71128797158fea6c9fb713c4079f62
momoysm <90163578+momoysm@users.noreply.github.com>
2024-08-21 17:01:19 +0900

    🐛 bugfix(#322): 공연장 최대좌석수 음수입력 불가능

--- sycls-admin-api/src/main/java/com/b1/place/dto/PlaceAddRequestDto.java
+++ sycls-admin-api/src/main/java/com/b1/place/dto/PlaceAddRequestDto.java
@@ -1,5 +1,7 @@
 package com.b1.place.dto;
 
+import jakarta.validation.constraints.Max;
+import jakarta.validation.constraints.Min;
 import jakarta.validation.constraints.NotBlank;
 import jakarta.validation.constraints.NotNull;
 import jakarta.validation.constraints.Size;
@@ -14,6 +16,8 @@ public record PlaceAddRequestDto(
         String name,
 
         @NotNull(message = "공연장 최대 좌석수가 누락되었습니다.")
+        @Max(value = 1000000000, message = "최대 좌석수는 10억석을 넘을 수 없습니다.")
+        @Min(value = 0, message = "최대 좌석수는 0보다 작을 수 없습니다.")
         Integer maxSeat
 ) {
 
